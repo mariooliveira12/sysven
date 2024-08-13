c087e1f5060d7e289123bb7a05e2b2ea2c916d34
Mário Batista <mario.batista.oliveira3@gmail.com>
2024-08-13 11:19:10 -0300

    fix: Correcao de bugs

--- php/controller/cliente.php
+++ php/controller/cliente.php
@@ -1,7 +1,10 @@
 <?php
      include_once '_sessao.php';
+
      isSession();
+
      include '../model/usuario_model.php';
+     
      include '../view/header.php';
 
      include '../view/usuario/index.php';
